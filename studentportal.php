054e1ea91894d6266cdb27c9146b95c4f6142ca5
Timbo <timothy_amis@msn.com>
2021-05-10 15:15:08 +0100

    student portal title fix

--- studentportal.php
+++ studentportal.php
@@ -87,7 +87,7 @@ if (!isset($_SESSION['studentID'])) {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>placeholder students Portal</title>
+        <title>Students Portal</title>
         <link href="css/normalize.css" rel="stylesheet" />
         <link href="css/stylesheet.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap" rel="stylesheet">
