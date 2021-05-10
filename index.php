02399fc23b329ed78a7b2a3c91a7a4fa974d6fd3
Timbo <timothy_amis@msn.com>
2021-05-10 15:04:20 +0100

    Fixed up the marketplace products not appearing database connection issue

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php session_start(); ?>
+<?php session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,7 +25,6 @@
             <!-- Left start -->
             <div class="leftgrid">
                 <?php include 'sidebar.php'?>
-                   
             </div>
             <!-- Left end -->
             <!-- right start -->
@@ -29,20 +32,29 @@
             <!-- Body start -->
             <div class="bodygrid">
                 <?php
-                $connection = mysqli_connect('localhost', 'root', ''); //The Blank string is the password
-                mysqli_select_db($connection, 'db');
-                $query = "SELECT * FROM marketplace";
-                $result = mysqli_query($connection, $query);
-                $array = array();
-                while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-                    $array[] = $row;
+                require 'php/dbh.inc.php';
+                if (isset($conn)) {
+                    $stmt = mysqli_stmt_init($conn);
+                }else{
+                    header("../index.php?error=mysqlerror");
+                    exit();
                 }
-    
-                $fp = fopen('products.json', 'w');
-                fwrite($fp, print_r(json_encode($array), TRUE));
-                fclose($fp);
-                
-                mysqli_close($connection); //Make sure to close out the database connection
+                $query = "SELECT * FROM marketplace";
+                if (!mysqli_stmt_prepare($stmt, $query)) {
+                    header("location: ../login.php?error=sqlerror");
+                    exit();
+                }else{
+                    $result = mysqli_query($conn, $query);
+                    $array = array();
+                    while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
+                        $array[] = $row;
+                    }
+                    $fp = fopen('products.json', 'w');
+                    fwrite($fp, print_r(json_encode($array), TRUE));
+                    fclose($fp);
+
+                    mysqli_close($conn); //Make sure to close out the database connection
+                }
                     ?>
                 <ul class = cards id="charactersList"></ul>
                 <!-- Footer start -->
